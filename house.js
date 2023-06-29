8e2ac5e35231cee4b7ed4f20370fdc4fcf0625b9
beka <126388438+t0lonovv@users.noreply.github.com>
2023-06-29 12:06:12 +0600

    Add files via upload

---
+++ house.js
@@ -0,0 +1,125 @@
+
+
+
+
+let b1 = document.querySelector("#b1")
+let lift =document.querySelector('#lift')
+b1.addEventListener('click', ()=> {
+    lift.style.top = "790px"
+})
+
+
+let b2 = document.querySelector("#b2")
+// let lift =document.querySelector('#lift')
+b2.addEventListener('click', ()=> {
+    lift.style.top = "740px"
+})
+
+
+let b3 = document.querySelector("#b3")
+// let lift =document.querySelector('#lift')
+b3.addEventListener('click', ()=> {
+    lift.style.top = "690px"
+})
+
+
+let b4 = document.querySelector("#b4")
+// let lift =document.querySelector('#lift')
+b4.addEventListener('click', ()=> {
+    lift.style.top = "640px"
+})
+
+
+let b5 = document.querySelector("#b5")
+// let lift =document.querySelector('#lift')
+b5.addEventListener('click', ()=> {
+    lift.style.top = "590px"
+})
+
+
+let b6 = document.querySelector("#b6")
+// let lift =document.querySelector('#lift')
+b6.addEventListener('click', ()=> {
+    lift.style.top = "540px"
+})
+
+
+let b7 = document.querySelector("#b7")
+// let lift =document.querySelector('#lift')
+b7.addEventListener('click', ()=> {
+    lift.style.top = "490px"
+})
+
+
+let b8 = document.querySelector("#b8")
+// let lift =document.querySelector('#lift')
+b8.addEventListener('click', ()=> {
+    lift.style.top = "440px"
+})
+
+
+let b9 = document.querySelector("#b9")
+// let lift =document.querySelector('#lift')
+b9.addEventListener('click', ()=> {
+    lift.style.top = "390px"
+})
+
+
+let b10 = document.querySelector("#b10")
+// let lift =document.querySelector('#lift')
+b10.addEventListener('click', ()=> {
+    lift.style.top = "340px"
+})
+
+
+let b11 = document.querySelector("#b11")
+// let lift =document.querySelector('#lift')
+b11.addEventListener('click', ()=> {
+    lift.style.top = "290px"
+})
+
+
+let b12 = document.querySelector("#b12")
+// let lift =document.querySelector('#lift')
+b12.addEventListener('click', ()=> {
+    lift.style.top = "240px"
+})
+
+
+let b13 = document.querySelector("#b13")
+// let lift =document.querySelector('#lift')
+b13.addEventListener('click', ()=> {
+    lift.style.top = "190px"
+})
+
+
+let b14 = document.querySelector("#b14")
+// let lift =document.querySelector('#lift')
+b14.addEventListener('click', ()=> {
+    lift.style.top = "140px"
+})
+
+
+let b15 = document.querySelector("#b15")
+// let lift =document.querySelector('#lift')
+b15.addEventListener('click', ()=> {
+    lift.style.top = "90px"
+})
+
+
+let b16 = document.querySelector("#b16")
+// let lift =document.querySelector('#lift')
+b16.addEventListener('click', ()=> {
+    lift.style.top = "40px"
+})
+
+
+
+
+
+
+
+
+
+
+
